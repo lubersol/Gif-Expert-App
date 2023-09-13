3f252011183e44f9b9abc5f86adfb507ad75e05d
lubersol <lubersol@gmail.com>
2023-09-13 14:28:29 +0200

    añadido hook useState para categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,22 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { useState } from 'react';
+
 
 export const GifExpertApp = () => {
+
+    // eslint-disable-next-line no-unused-vars
+    const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+
     return (
         <>
             <h1>GifExpertApp</h1>
+            <ol>
+                {
+                categories.map( category => {
+                    return <li key={ category }>{ category }</li>
+                })
+                }
+            </ol>
         </>
     )
 }
